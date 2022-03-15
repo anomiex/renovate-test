2017f608e9d3283da4375f8467d2d9ea2db06c6a
Brad Jorsch <anomie@users.sourceforge.net>
2022-03-15 13:43:52 -0400

    Try with separate renovate.json

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -4,18 +4,5 @@ module.exports = {
 	platform: 'github',
 	repositories: [ 'anomiex/renovate-test' ],
 
-	// We're including configuration in this file.
 	onboarding: false,
-	requireConfig: false,
-
-	// This is the renovate configuration.
-	extends: [ 'config:base' ],
-	timezone: 'UTC',
-	dependencyDashboard: true,
-	packageRules: [
-		{
-			extends: [ 'monorepo:wordpress' ],
-			separateMajorMinor: false,
-		},
-	],
 };
